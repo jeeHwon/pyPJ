a46a15b48d87f7b438dcfa478a44a4112cb024e8
jeeHwon <jeehwon01@gmail.com>
2020-11-24 20:56:04 +0900

    ADD 1124

---
+++ stockPJ/ch06/SecondScreen.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
+from Investar import Analyzer
+
+mk = Analyzer.MarketDB()
+df = mk.get_daily_price('엔씨소프트', '2017-01-01')
+
+ema60 = df.close.ewm(span=60).mean()    # 종가의 12주 지수 이동평균
+ema130 = df.close.ewm(span=130).mean()    # 종가의 26주 지수 이동평균
+macd = ema60 - ema130   # MACD선
+signal = macd.ewm(span=45).mean()   # 신호선(MACD의 9주 지수이동평균)
+macdhist = macd - signal    # MACD 히스토그램
+
+df = df.assign(ema130=ema130, ema60=ema60, macd=macd, signal=signal, macdhist=macdhist).dropna()
+df['number'] = df.index.map(mdates.date2num)
+ohlc = df[['number','open','high','low','close']]
+
+ndays_high = df.high.rolling(window=14, min_periods=1).max()
+ndays_low = df.low.rolling(window=14, min_periods=1).min()
+fast_k = (df.close - ndays_low) / (ndays_high - ndays_low) * 100
+slow_d = fast_k.rolling(window=3).mean()
+df = df.assign(fast_k=fast_k, slow_d=slow_d).dropna()
+
+plt.figure(figsize=(9,7))
+p1 = plt.subplot(2,1,1)
+plt.title('Triple Screen Trading - Second Screen (NCSOFT)')
+plt.grid(True)
+candlestick_ohlc(p1, ohlc.values, width=.6, colorup='red', colordown='blue')
+p1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+plt.plot(df.number, df['ema130'], color='c', label='EMA130')
+plt.legend(loc='best')
+
+p2 = plt.subplot(2,1,2)
+plt.grid(True)
+p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+plt.plot(df.number, df['fast_k'], color='c', label='%K')
+plt.plot(df.number, df['slow_d'], color='k', label='%D')
+plt.yticks([0, 20, 80, 100])
+plt.legend(loc='best')
+plt.show()
+
+
+
