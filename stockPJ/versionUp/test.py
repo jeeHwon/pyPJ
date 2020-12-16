a97095da01829a2c0739ba7ba68e5b5cc6406957
jeeHwon <jeehwon01@gmail.com>
2020-12-16 19:06:47 +0900

    DELETE trash folder

--- stockPJ/versionUp/test.py
+++
@@ -1,9 +0,0 @@
-import csv
-symbol_list = ['A122630', 'A252670', 'A233740', 'A250780', 'A225130', 'A280940', 'A261220', 'A217770', 'A295000', 'A176950']
-# csv 파일 읽기 
-with open('C:/myApp/Investar/BB_TF_buylist.csv', 'r', encoding='utf-8') as f:
-    rdr = csv.reader(f) 
-    for i,line in enumerate(rdr): 
-        if i==0: 
-            symbol_list.extend(line)
-print(symbol_list)
