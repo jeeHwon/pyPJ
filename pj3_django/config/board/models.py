fdfbdaee919144611733202bea26e17793780e40
jeeHwon <jeehwon01@gmail.com>
2021-01-05 16:09:52 +0900

    ADD 210105 study django

---
+++ pj3_django/config/board/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# class Board(models.Model):
+#     title = models.CharField(max_length=10)
+#     username = models.CharField(max_length=10)
+#     pw = models.CharField(max_length=10)
+#     regdate = models.DateTimeField(auto_now_add=True)
