b20a5e130524393c8e805ea4fb22b9320fa384b4
jeeHwon <jeehwon01@gmail.com>
2021-01-22 23:08:10 +0900

    ADD 210122 study

--- pj2_conda/pandas36.py
+++ pj2_conda/pandas36.py
@@ -1,5 +1,5 @@
 import seaborn as sns
-# 과제 : tips 데이터를 이용하여 요일을 행인덱스, 성별을 열인덱스로 하여 식사금액의 합을 출력
+# 과제 : tips데이터를 활용하여 성별 요일별 식대의 합을 출력하세요(피벗테이블 이용하세요)
 tips = sns.load_dataset('tips')
 tips_pivot = tips.pivot_table(index='day', columns='sex', values='total_bill' ,aggfunc='sum')
 print(tips_pivot)
