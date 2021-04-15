a37efd9e7f7000528300ebeedace427fed19876e
jeeHwon <jeehwon01@gmail.com>
2021-04-15 14:23:20 +0900

    ADD 0415 study

--- coin/ch06/06_01.py
+++ coin/ch06/06_01.py
@@ -8,3 +8,6 @@ upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회
+
+balance = upbit.get_balance("BTC")
+print(balance)
